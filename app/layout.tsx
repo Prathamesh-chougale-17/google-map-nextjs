3b2f1acf5aa8940bf78f90a425a64964bd5b2754
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-12-12 08:56:10 +0530

    Add dark mode support and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
         <Providers>
           <FixedNavbar />
